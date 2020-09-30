9e4856fd1bd40991861bbc7f01bd5c52ddbf9e05
sebafm <seba.ffm@gmail.com>
2020-09-30 10:59:09 +0200

    Commit A on branch reset_branch

---
+++ NewTestFile1.py
@@ -0,0 +1,9 @@
+""" This is the first file in the new branch. """
+
+
+Explore California!
+More Code in test file 1.
+
+Hier kommt eine Änderung für den ersten Mixed Reset.
+
+Nachdem ich den Mixed Reset durchgeführt habe, füge ich eine neue Zeile hinzu.
